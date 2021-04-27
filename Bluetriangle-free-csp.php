74c29ca76f2b32397713d0e3c3adb257aa5a2131
Andrew-HT <andrew.harsono@bluetriangle.com>
2021-04-27 14:29:42 -0400

    enhancement #31 Fixed wrong table referenced in db update function

--- Bluetriangle-free-csp.php
+++ Bluetriangle-free-csp.php
@@ -1061,7 +1061,6 @@ function Blue_Triangle_Automated_CSP_Free_update_db_check() {
     global $wpdb;
     
     if ($pluginVersion == false) {
-        print_r("No plugin version!!!");
         //if there was no previous plugin version update schema to current version 
         delete_option( 'Blue_Triangle_Automated_CSP_Free_Directives');
         delete_option( 'Blue_Triangle_Automated_CSP_Free_Directive_Options');
@@ -1072,14 +1071,12 @@ function Blue_Triangle_Automated_CSP_Free_update_db_check() {
         Blue_Triangle_Automated_Free_CSP_install();
     }
     if ($pluginVersion == 'No site prefix') {
-        print_r("No site Prefix!!!");
-        $wpdb->query("ALTER TABLE `seasp_directive_settings` RENAME TO `".$wpdb->prefix."seasp_directive_settings`");
+        $wpdb->query("ALTER TABLE `seasp_site_settings` RENAME TO `".$wpdb->prefix."seasp_site_settings`");
         $pluginVersion = Blue_Triangle_Automated_CSP_Free_Get_Setting("plugin_version",$siteID);
     }
 
     $pluginVersion += 0.0;
     if($pluginVersion < 1.5){
-        print_r("1.5!!!");
         Blue_Triangle_Automated_CSP_Free_Update_Setting("plugin_version","1.5",$siteID);
         
         $insertStatement = 'insert into `'.$wpdb->prefix.'seasp_site_settings`(`site_id`,`setting_name`,`setting_value`) values ';
@@ -1090,9 +1087,7 @@ function Blue_Triangle_Automated_CSP_Free_update_db_check() {
             print_r($report);
         }
     }
-    if($pluginVersion < 1.8){
-        print_r("1.8!!!");
-        
+    if($pluginVersion < 1.8){        
         $charset_collate = $wpdb->get_charset_collate();
         //verified created 
         $sql = "CREATE TABLE " . $wpdb->prefix."seasp_subdomain_log (
@@ -1112,12 +1107,11 @@ function Blue_Triangle_Automated_CSP_Free_update_db_check() {
         $oldTables = $wpdb->query('SHOW TABLES LIKE "seasp_%"');
 
         if ($oldTables != 0) {
-            print_r("Altering " . $oldTables . " tables!!!");
             $wpdb->query("ALTER TABLE `seasp_allowed_plugins` RENAME TO `".$wpdb->prefix."seasp_allowed_plugins`");
             $wpdb->query("ALTER TABLE `seasp_csp` RENAME TO `".$wpdb->prefix."seasp_csp`");
             $wpdb->query("ALTER TABLE `seasp_directives` RENAME TO `".$wpdb->prefix."seasp_directives`");
             $wpdb->query("ALTER TABLE `seasp_directive_options` RENAME TO `".$wpdb->prefix."seasp_directive_options`");
-            $wpdb->query("ALTER TABLE `seasp_site_settings` RENAME TO `".$wpdb->prefix."seasp_site_settings`");
+            $wpdb->query("ALTER TABLE `seasp_directive_settings` RENAME TO `".$wpdb->prefix."seasp_directive_settings`");
             $wpdb->query("ALTER TABLE `seasp_sand_box_urls` RENAME TO `".$wpdb->prefix."seasp_sand_box_urls`");
             $wpdb->query("ALTER TABLE `seasp_violation_log` RENAME TO `".$wpdb->prefix."seasp_violation_log`");
         }
